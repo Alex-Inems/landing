cabe0869ca342537b584a10953084023f119ea27
Alex <inema9886@gmail.com>
2025-03-21 22:10:30 +0100

    Initial commit

---
+++ src/app/components/Hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+    return (
+      <nav className="p-4 bg-blue-600 text-white">
+        <h1 className="text-xl font-bold">My Landing Page</h1>
+      </nav>
+    );
+  }
+  
